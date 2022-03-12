746ea56f481eb0f62e60ef0909f392580d5cf297
ALFROON <96896741+ALFROON@users.noreply.github.com>
2022-03-11 22:44:12 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 def send_sms():
     """ACfc44d25ff122584fc5b5679aeaea6de4"""
- auth_token = 'a2cb8ddb1fe73b4c57758bc3af2105c8'
+
+
+auth_token = 'a2cb8ddb1fe73b4c57758bc3af2105c8'
 client = Client(account_sid, auth_token)
 message = client.messages.create(
     from_='your_from_num',
